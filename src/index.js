9a499fd86d7594d24957ad1fd085b402b7d13a93
Arnab Sagar <arnab.sagar47@gmail.com>
2022-01-21 03:29:55 -0500

    added README + code Cleanup

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ var savePixels = require("save-pixels")
 const { spawn } = require('child_process');
 
 
-
-
-
-
 function convertFrameToPixels(myBuffer){ 
 
     let someConst;
@@ -37,7 +33,7 @@ async function getPixelData(path){
 
 function createVideo(pathToFrames){
 
-    const ls = spawn('ffmpeg', ['-i', `${pathToFrames}/output_%d.jpg`, '-vcodec', 'mpeg4', 'final.avi']);
+    const ls = spawn('ffmpeg', ['-i', `${pathToFrames}/output_%d.jpg`, '-vcodec', 'mpeg4', 'final.mp4']);
     
     ls.stdout.on('data', (data) => {
       console.log(`stdout: ${data}`);
@@ -54,22 +50,7 @@ function createVideo(pathToFrames){
 }
 
 
-function workFunction(pixelData){
 
-    // debugger
-    // console.log(pixelData);
-    progress();
-    // pixelData = pixelData.data
-    for(var i = 0; i<pixelData.length; i=i+4){
-        var total = (pixelData[i] + pixelData[i+1] + pixelData[i+2])/3
-        pixelData[i] = total
-        pixelData[i+1] = total
-        pixelData[i+2] = total
-        // console.log(i);
-    }
-
-    return pixelData
-}
 
 function createFrames(pathToVideo){
 
@@ -104,6 +85,26 @@ function createFrames(pathToVideo){
 
 }
 
+function workFunction(pixelData){
+    
+    // progress();
+    
+    for(var i = 0; i<pixelData.length; i=i+4){
+
+        /** For Grayscale */
+        var total = (pixelData[i] + pixelData[i+1] + pixelData[i+2])/3
+        pixelData[i] = total
+        pixelData[i+1] = total
+        pixelData[i+2] = total
+
+        /** For contrast (2) and brightness (50) */
+        // pixelData[i] =  (2*pixelData[i]) + 50;
+ 
+    }
+
+    return pixelData
+}
+
 
 async function main(){    
 
@@ -114,57 +115,62 @@ async function main(){
     let framesPixelsArray = []
 
     /* INPUT SET */  
-    // await createFrames(pathToVideo)//Split the video into frames
-    let frames = fs.readdirSync(pathToFrames)    
-    for(const frame of frames){             //Taking each frame in a loop. Converting the frame to pixel data.
-        // console.log(frame);                                                           //BUG FOUND  - WHILE READING FILE NAMES
+    await createFrames(pathToVideo)                 //Split the video into frames
+
+    let frames = fs.readdirSync(pathToFrames)       //Read list of frames and sort them in ascending order
+    frames.sort(function(a, b){
+        return a.length - b.length;
+    });
+
+    for(const frame of frames){                     //Taking each frame in the list and converting the frame to pixel data buffer object.
         let pixelData = await getPixelData(`${pathToFrames}/${frame}`)
         framesObjects.push(pixelData)    
     }
-    framesObjects.forEach(element => {      //Add the pixel data of frames to a list from the object data of frames...to make input set 
+
+    framesObjects.forEach(element => {              //Adding only the Uint8 pixel data of frames to a list from the object  
         framesPixelsArray.push(new Uint8Array(element.data))
     });
-    console.log(framesPixelsArray);
 
-    // framesPixelsArray.forEach(obj => {
-    //     console.log("OLD PIXELS DATA: \n", new Uint8Array(obj));
+    // framesPixelsArray.forEach(element => {
+    //     console.log(element);
     // });
 
+
     /*DCP COMPUTE*/ 
-    const compute = require('dcp/compute');
-    const job = compute.for(framesPixelsArray, workFunction); 
+    // const compute = require('dcp/compute');
+    // const job = compute.for(framesPixelsArray, workFunction); 
 
-    job.on('accepted', () => {
-        console.log(` - Job accepted by scheduler, waiting for results`);
-        console.log(` - Job has id ${job.id}`);
-        startTime = Date.now();
-    });
+    // job.on('accepted', () => {
+    //     console.log(` - Job accepted by scheduler, waiting for results`);
+    //     console.log(` - Job has id ${job.id}`);
+    //     startTime = Date.now();
+    // });
 
-    job.on('readystatechange', (arg) => {
-        console.log(`new ready state: ${arg}`);
-    });
+    // job.on('readystatechange', (arg) => {
+    //     console.log(`new ready state: ${arg}`);
+    // });
 
-    job.on('result', (ev) => {
-        console.log(
-            ` - Received result for slice ${ev.sliceNumber} at ${Math.round((Date.now() - startTime) / 100) / 10
-            }s`,
-        );
-    });
+    // job.on('result', (ev) => {
+    //     console.log(
+    //         ` - Received result for slice ${ev.sliceNumber} at ${Math.round((Date.now() - startTime) / 100) / 10
+    //         }s`,
+    //     );
+    // });
   
-    job.public.name = 'Arnab\'s intern project, nodejs ';
+    // job.public.name = 'Arnab\'s intern project, nodejs ';
 
-    let resultSet = await job.localExec(); //for executing the job locally OR
+    // let resultSet = await job.localExec(); //for executing the job locally OR
     // let resultSet = await job.exec(0.001); //for executing the job on DCP
     
     // // /* PROCESSING OF RESULTS */ 
-    resultSet = Array.from(resultSet);
-    console.log(resultSet);
-    let counter1 = 0
-    resultSet.forEach(function (frame) {
-        framesObjects[counter1].data = Array.from(frame)
-        counter1++;
-    });
-    console.log(framesObjects);
+    // resultSet = Array.from(resultSet);
+    // console.log(resultSet);
+    // let counter1 = 0
+    // resultSet.forEach(function (frame) {
+    //     framesObjects[counter1].data = Array.from(frame)
+    //     counter1++;
+    // });
+    // console.log(framesObjects);
 
     // console.log("New pixels data after the work function:");
     // framesObjects.forEach(element => {
@@ -172,26 +178,26 @@ async function main(){
     // });
 
     /* SOLUTION FOR NON-DCP RESULTS*/
-    // for (let frame of framesPixelsArray) {
-    //     frame = workFunction(frame);
-    // }
-    // for (var i = 0; i < framesObjects.length; i++) {    //Copying resulting pixel data of frames back to the list of object data of frames
-    //     framesObjects[i].data = framesPixelsArray[i];
-    // }
+    for (let frame of framesPixelsArray) {
+        frame = workFunction(frame);
+    }
+    for (var i = 0; i < framesObjects.length; i++) {    //Copying resulting pixel data of frames back to the list of object data of frames
+        framesObjects[i].data = framesPixelsArray[i];
+    }
 
     
     /* SAVING CONVERTED FRAMES' OBJECTS TO FRAMES' IMAGES */
     let counter = 0;    
     for(const frame of framesObjects){      //Loop to save each frame's object data as an image
         counter++;
-        console.log(frame);
+        // console.log(frame);
         let bufferOut = await savePixels(frame, 'jpg'); // ndarray -> Uint8Array
         fs.writeFileSync(`./outputSet/output_${counter}.jpg`, bufferOut._obj);
     }
 
 
     /* Stiching resulting frames to a video */
-    // createVideo(pathToConvertedFrames);
+    createVideo(pathToConvertedFrames);
 
 }
 
